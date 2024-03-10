ef3339649a80b4fd398d512453c6940b719b79c1
Sudharshan Vijay SK <98068200+Spark1810@users.noreply.github.com>
2024-03-10 18:51:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 import streamlit as st
 import pandas as pd
-import streamlit as st
-import pandas as pd
 from PIL import Image
 import time
-import streamlit as st
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 # Security
 #passlib,hashlib,bcrypt,scrypt
